cf9bddcac2de42f704728aa3b61d0f4487408979
coologic <techieliang@qq.com>
2022-07-09 15:56:40 +0800

    [init] feat: init project

---
+++ cacaocat/src/main/java/org/coologic/cacaocat/CacaocatApplication.java
@@ -0,0 +1,13 @@
+package org.coologic.cacaocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaocatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacaocatApplication.class, args);
+    }
+
+}
---
+++ cacaocat/src/test/java/org/coologic/cacaocat/CacaocatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coologic.cacaocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaocatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
