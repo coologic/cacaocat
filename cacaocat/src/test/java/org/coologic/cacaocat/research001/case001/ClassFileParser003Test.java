b5a7f8dfeeefc019177e594d4f98086dc4930a8b
coologic <techieliang@qq.com>
2022-07-20 18:50:53 +0800

    [instraction] feat: switch type

---
+++ cacaocat/src/test/java/org/coologic/cacaocat/research001/case001/ClassFileParser003Test.java
@@ -0,0 +1,22 @@
+package org.coologic.cacaocat.research001.case001;
+
+import org.coologic.cacaocat.research001.case001.domain.ClassFile;
+import org.junit.jupiter.api.Test;
+
+import java.io.DataInput;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ClassFileParser003Test {
+    @Test
+    void testParse() throws IOException {
+        String path = System.getProperty("user.dir") + "/src/test/resources/bytecode/case003_switchInstruction.class";
+        DataInput dataInput = FileReader.readClassFile(path);
+        ClassFile classFile = ClassFileParser.parseClassFile(dataInput);
+        assertNotNull(classFile);
+        String s = classFile.toString();
+        assertEquals(0xCAFEBABE, classFile.getMagic());
+    }
+}
